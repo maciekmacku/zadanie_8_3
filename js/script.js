dea2b02db7ac20f5a45def2f3149517c0eb41cb3
maciekmacku <maciejszalaj@gmail.com>
2018-03-29 22:57:33 +0200

    added script and test with jshint

---
+++ js/script.js
@@ -0,0 +1,7 @@
+// enter your name :) 
+var name = prompt('Enter your name');
+alert('Hello, ' + name);
+console.log('Hello, ' + name);
+// npm test 
+// don't look at nu-html-checker -> no index.html
+// npm jshint's working fine
